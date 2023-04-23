6cd849e13658435ef082c917d2302be8afab152a
Atsushi <atu729@icloud.com>
2023-04-23 23:11:45 +0900

    add basic page image

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
+import styled from "styled-components"
+
 import Hero from "./components/Hero";
 import Who from "./components/Who";
 import Works from "./components/Works";
 import Contact from "./components/Contact";
 
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-type: y mandatory;
+  scroll-behavior: smooth;
+  overflow-y: auto;
+  scrollbar-width: none;
+  color: white;
+  background: url("./img/bg.jpeg");
+  &::-webkit-scrollbar{
+    display: none;
+  }
+`;
+
 function App() {
 
   return (
-    <div>
+    <Container>
       <Hero></Hero>
       <Who></Who>
       <Works></Works>
       <Contact></Contact>
-    </div>
+    </Container>
   )
 }
 
