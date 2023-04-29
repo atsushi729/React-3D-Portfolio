f6429b8207d1367d9fd19c4a1d57739761a70298
Atsushi <atu729@icloud.com>
2023-04-30 02:15:57 +0900

    add Who componens

--- src/components/Who.jsx
+++ src/components/Who.jsx
@@ -1,15 +1,97 @@
 import React from "react";
-import styled from "styled-components"
+import styled from "styled-components";
 
 const Section = styled.div`
   height: 100vh;
-  scroll-snap-align:center;
-`
+  scroll-snap-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  height: 100%;
+  scroll-snap-align: center;
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+`;
+
+const Left = styled.div`
+  flex: 1;
+
+  @media only screen and (max-width: 768px) {
+    flex: 1;
+    align-items: center;
+  }
+`;
+
+const Title = styled.h1`
+  font-size: 74px;
+`;
+
+const WhatWeDo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Line = styled.img`
+  height: 5px;
+`;
+
+const Subtitle = styled.h2`
+  color: #da4ea2;
+`;
+
+const Desc = styled.p`
+  font-size: 24px;
+  color: lightgray;
+  @media only screen and (max-width: 768px) {
+    padding: 20px;
+    text-align: center;
+  }
+`;
+
+const Button = styled.button`
+  background-color: #da4ea2;
+  color: white;
+  font-weight: 500;
+  width: 100px;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
 
 const Who = () => {
-    return (
-        <Section>Who</Section>
-    )
-}
+  return (
+    <Section>
+      <Container>
+        <Left>3D model</Left>
+        <Right>
+          <Title>Think outside the square space</Title>
+          <WhatWeDo>
+            <Line src="./img/line.png" />
+            <Subtitle>Who we Are</Subtitle>
+          </WhatWeDo>
+          <Desc>
+            a creative group of designers and developers with a passion for the
+            arts.
+          </Desc>
+          <Button>See our works</Button>
+        </Right>
+      </Container>
+    </Section>
+  );
+};
 
-export default Who
+export default Who;
